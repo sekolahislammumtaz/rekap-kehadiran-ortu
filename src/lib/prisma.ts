9ae1541bc963c9bd647453f979753c3814cf06c9
sekolahislammumtaz <smpimumtaz@gmail.com>
2026-08-05 15:56:25 +0700

    Add files via upload

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,4 +10,5 @@ export const prisma =
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Always reuse PrismaClient instance across serverless function warm lambdas in production
+globalForPrisma.prisma = prisma;
